6afe44dd9ba71a21a4154e5e7854c1e7d3cdb33b
nihollatech <shadeniola@gmai.com>
2022-09-20 13:45:04 -0700

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* reset_to_98 - function that takes a pointer to an int
+* @n: value to be evaluated
+* Return (0)
+*/
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
