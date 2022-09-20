616d5f8e20cc91c06b3fd214864470ff4f717336
nihollatech <shadeniola@gmai.com>
2022-09-20 15:35:31 -0700

    okay

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,15 +5,14 @@
 * @s: value to be evaluated.
 * Return: not
 */
+
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		len++;
-	
-	}			
-	return (len);
-}	
+int len = 0;
+while (*s != '\0')
+{
+	s++;
+	len++;
+}
+return (len);
+}
