c0891654a5f202897435467a73e162eb14ae7d57
nihollatech <shadeniola@gmai.com>
2022-09-20 15:19:22 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* _strlen - a function thta returns length of a string.
+* @s: value to be evaluated.
+* Return: not
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	
+	}			
+	return (len);
+}	
