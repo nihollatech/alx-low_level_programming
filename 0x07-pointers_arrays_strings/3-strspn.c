65efd5998e7abc205c3cbab799e8417bc3e6d276
nihollatech <shadeniola@gmai.com>
2022-09-26 20:52:33 -0700

    fifth task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/**
+/*
 * _strchr - Locates a character in a string
 * @s: String
 * @c: Character to search
 * Return: Pointer to the first occurrence of the character c in the string s
-**/
+*/
 
 char *_strchr(char *s, char c)
 
