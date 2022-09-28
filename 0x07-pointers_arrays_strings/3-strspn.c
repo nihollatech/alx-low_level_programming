ae8e5cc8b092a98e07870437de27f1520375c763
nihollatech <shadeniola@gmai.com>
2022-09-27 20:20:50 -0700

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,8 +13,8 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int length;
 
 	length = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
+	for (i = 0; s[i] != '\0'; i++
+		{
 			for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
 				;
 			if (s[i] == accept[j])
