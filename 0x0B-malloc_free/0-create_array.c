e28bd0f60efb03ce5462056bbc54eeb7e05db1ad
nihollatech <shadeniola@gmai.com>
2022-10-05 21:46:05 -0700

    whitesoace

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,11 +16,11 @@ char *create_array(unsigned int size, char c)
 {
 	char *arr;
 	unsigned int i;
-	
+
 	if (size == 0)
 		return ('\0');
 	arr = malloc(size);
-	
+
 	if (arr == NULL)
 		return ('\0');
 	
